2e324861715b3da15eb09174df26894001fcb331
Virhan Ali Hameini <34044859+virhanali@users.noreply.github.com>
2019-01-06 16:23:08 +0700

    Add files via upload

---
+++ blog/functions/user.php
@@ -0,0 +1,18 @@
+<?php
+
+function cek_data($nama, $pass){
+ $nama = escape($nama);
+ $pass = escape($pass);
+
+
+ $query = "SELECT * FROM users WHERE username='$nama' AND password='$pass'";
+ global $link;
+
+ if($result= mysqli_query($link, $query)){
+   if(mysqli_num_rows($result) != 0)return true;
+   else return false;
+ }
+
+}
+
+ ?>
---
+++ blog/view/footer.php
@@ -0,0 +1,3 @@
+<p id="footer">
+  Blog Sistem Informasi
+</p>
